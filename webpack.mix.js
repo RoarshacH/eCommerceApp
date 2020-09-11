47395bbbd56de68f5b563273a15aed27ed282713
RoarshacH <smv.dilshan@gmail.com>
2020-09-11 07:47:17 +0530

    webpack and other

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,21 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+mix.styles([
+    'public/css/vendor/nouislider.min.css',
+    'public/css/vendor/slick.css',
+    'public/css/vendor/slick-theme.css',
+    'public/css/vendor/style.css'
+], 'public/css/all.css');
+
+mix.scripts([
+    'public/js/vendor/jquery.zoom.min.js',
+    'public/js/vendor/nouislider.min.js',
+    'public/js/vendor/slick.min.js',
+    'public/js/vendor/main.js'
+], 'public/js/all.js');
+
+// 'public/js/vendor/bootstrap.min.js',
+//  'public/js/vendor/jquery.min.js',
+
